f4b6bf90b4765670fd2c0db8efb48b8a00cb0003
Adrian Piedade <102343274+AdrianPiedade@users.noreply.github.com>
2023-11-23 03:15:56 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function buscarCEP() {
+
+    var cep = document.getElementById('cep').value;
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep.length !== 8) {
+        alert('Por favor, insira um CEP válido com 8 dígitos.');
+        return;
+    }
+
+    var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            Object.keys(data).forEach(key => {
+                if (!data[key]) {
+                    data[key] = "Não informado";
+                }
+            });
+
+            document.getElementById('logradouro').value = data.logradouro;
+            document.getElementById('complemento').value = data.complemento;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('localidade').value = data.localidade;
+            document.getElementById('uf').value = data.uf;
+            document.getElementById('ddd').value = data.ddd
+
+            console.log('JSON de retorno:', data);
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+            alert('Erro ao buscar o CEP. Por favor, tente novamente.');
+        });
+}
